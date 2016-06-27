b1e4a95442c0ccafa235f7d47ce7a158e411aa53
Dilpreet Johal <dilpreet.johal022@gmail.com>
2016-06-27 19:28:57 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+    requireAuthentication: function (req, res, next) {
+        console.log('Private route hit!');
+        next();
+    },
+    logger: function (req, res, next) {
+        var date = new Date(yyyy-mm-dd);
+        console.log('Request: ' + req.method + ' ' + req.originalUrl + ' ' + date);
+        next();
+    }
+}
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+    res.send('About Us!');
+})
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, function () {
+    console.log('Express server started at ' + PORT + '!');
+});
